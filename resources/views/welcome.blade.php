3d61072437e3ff77785459dd3d948a6c7c23d760
LinJoseph <lyz2356002@gmail.com>
2016-06-04 12:37:50 +0800

    去除了todo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row jumbotron">
-           <h1> 欢迎使用健康监测系统</h1>
-            <p>您可以通过定时输入自己的心跳血压观测自己的身体折线图</p>
+            <h1> 欢迎使用健康监测系统</h1>
+            <p>使用本系统对自己的身体健康进行监控</p>
         </div>
     </div>
 @endsection
